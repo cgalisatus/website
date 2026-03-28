b8d14b835a759f6d02b9620616185e2d2af7886f
Christina Galisatus <cmgalisatus@gmail.com>
2026-03-29 00:49:23 +0100

    show links

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const showsData = [
     { date: "2026-06-03", text: "June 3 - Brooklyn, NY - Three's Brewing" },
     { date: "2026-06-04", text: "June 4 - Brooklyn, NY - Mudhouse" },
     { date: "2026-06-06", text: "June 6 - Manhattan, NY - Close Up" },
-    { date: "2026-07-13", text: "July 13 - Stanford, CA - Stanford Jazz Festival" }
+    { date: "2026-07-13", text: "July 13 - Stanford, CA - Stanford Jazz Festival", link: "https://stanfordjazz.org/event/christina-galisatus-quintet-plays-joni-mitchell/" }
 ];
 
 const upcomingContainer = document.querySelector('.upcoming');
@@ -20,7 +20,13 @@ showsData.forEach(show => {
     const showDate = new Date(year, month - 1, day); //include shows that are today
     const div = document.createElement('div');
     div.classList.add('show');
+
+    console.log("test");
+    if (show.link) {
+        div.innerHTML = `<p>${show.text} <a href="${show.link}" target="_blank">🎟️</a></p>`;
+    } else {
     div.innerHTML = `<p>${show.text}</p>`;
+    }
 
     if (showDate < today) {
         pastContainer.appendChild(div);
